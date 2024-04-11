d6012d71c5240ebb111aecd2863b160ada4bf15d
Michelle Mabuyo <michelle@apollographql.com>
2024-04-11 13:53:13 -0400

    lesson 3 - strawberry, graphqlrouter setup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 app = FastAPI()
 
+graphql_router = GraphQLRouter(..., path="/", graphql_ide="apollo-sandbox")
 
-@app.get("/")
-async def hello_world():
-    return {"message": "Hello World"}
+app.include_router(graphql_router)
