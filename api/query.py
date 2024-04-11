1da392527ecc6faaade2275aa86ee9578c3a018f
Michelle Mabuyo <michelle@apollographql.com>
2024-04-11 15:59:54 -0400

    lesson 10 - using data source in featured_playlists

--- api/query.py
+++ api/query.py
@@ -1,13 +1,21 @@
 import strawberry
+from mock_spotify_rest_api_client.api.playlists import get_featured_playlists
 from .types.playlist import Playlist
 
 
 @strawberry.type
 class Query:
     @strawberry.field(description="Playlists hand-picked to be featured to all users.")
-    def featured_playlists(self) -> list[Playlist]:
+    async def featured_playlists(self, info: strawberry.Info) -> list[Playlist]:
+        spotify_client = info.context["spotify_client"]
+
+        data = await get_featured_playlists.asyncio(client=spotify_client)
+
         return [
-            Playlist(id="1", name="GraphQL Groovin'", description=None),
-            Playlist(id="2", name="Graph Explorer Jams", description=None),
-            Playlist(id="3", name="Interpretive GraphQL Dance", description=None),
+            Playlist(
+                id=strawberry.ID(playlist.id),
+                name=playlist.name,
+                description=playlist.description,
+            )
+            for playlist in data.playlists.items
         ]
