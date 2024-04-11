3e0286d1d1aa2b056142852f9561c605ee7b65e1
Michelle Mabuyo <michelle@apollographql.com>
2024-04-11 15:57:33 -0400

    lesson 9 - setting up context with spotify_client

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from strawberry.fastapi import GraphQLRouter
+from contextlib import asynccontextmanager
+from mock_spotify_rest_api_client.client import Client
 from api.schema import schema
 
-app = FastAPI()
 
-graphql_router = GraphQLRouter(schema, path="/", graphql_ide="apollo-sandbox")
+@asynccontextmanager
+async def lifespan(app):
+    async with Client(
+        base_url="https://spotify-demo-api-fe224840a08c.herokuapp.com/v1"
+    ) as client:
+        yield {"spotify_client": client}
+
+
+async def context_getter(request: Request):
+    spotify_client = request.state.spotify_client
+    return {"spotify_client": spotify_client}
+
+
+app = FastAPI(lifespan=lifespan)
+
+graphql_router = GraphQLRouter(
+    schema, path="/", graphql_ide="apollo-sandbox", context_getter=context_getter
+)
 
 app.include_router(graphql_router)
